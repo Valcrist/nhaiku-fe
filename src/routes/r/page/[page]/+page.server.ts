a8db53d441e40a9949325ac1d34bebbaec7666e5
Valcrist <github@valcrist.com>
2026-04-22 11:27:52 +0800

    reverted thumbnail behavior for remote gallery

--- src/routes/r/page/[page]/+page.server.ts
+++ src/routes/r/page/[page]/+page.server.ts
@@ -1,16 +1,20 @@
 import { API_KEY, API_URL } from '$env/static/private';
 import { remoteSearchManga } from '$lib/server/api';
 import { parseSearchParams } from '$lib/server/search';
+import { getThumbServers } from '$lib/server/thumbCache';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, url }) => {
   const { page, query, sort } = parseSearchParams(params, url);
 
-  const gallery = await remoteSearchManga(API_URL, API_KEY, page, query, sort);
+  const [servers, gallery] = await Promise.all([
+    getThumbServers(API_URL),
+    remoteSearchManga(API_URL, API_KEY, page, query, sort),
+  ]);
 
   const items = gallery.result.map((item) => ({
     ...item,
-    thumbUrl: `/media/thumb/${item.thumbnail}`,
+    thumbUrl: `${servers[((item.id * 2654435761) >>> 0) % servers.length]}/${item.thumbnail}`,
   }));
 
   return {
