0e21a1c591f80a6b4e5e6270764f9548fb7ce8d0
Valcrist <github@valcrist.com>
2026-05-20 12:39:50 +0800

    wpa + explicit dark mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,ico,png,svg,woff2}'],
+        navigateFallback: null,
+      },
+    }),
+  ],
 });
